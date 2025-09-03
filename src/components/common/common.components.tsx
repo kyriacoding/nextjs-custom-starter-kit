bccb8288604fc1513b4d367ceb59387553c1edd4
kyriacoding <102484733+kyriacoding@users.noreply.github.com>
2025-09-03 13:58:52 +0200

    Refactor DisplayTitle component styling

--- src/components/common/common.components.tsx
+++ src/components/common/common.components.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 export function DisplayTitle({ title, icon }: { title: string; icon: React.ReactNode }) {
   return (
-    <h2 className="scroll-m-20 border-b p-2 text-3xl font-semibold tracking-tight mx-5 first:mt-0">
+    <h2 className="text-3xl font-semibold w-fit mx-auto bg-primary p-2 rounded-md text-white">
       <span className="flex items-center gap-2">
         {icon}
         {title.toUpperCase()}
       </span>
     </h2>
   )
-}
+
 
 // display inner title with icon
 export function DisplayInnerTitle({ title, icon }: { title: string; icon: React.ReactNode }) {
